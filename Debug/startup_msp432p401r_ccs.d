546b4d8f6169464f9b09ce2bec237e09953cdb7d
Tom Victor <vjtomvictor@icloud.com>
2017-02-06 09:21:44 +0530

    lib include problem

--- Debug/startup_msp432p401r_ccs.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-startup_msp432p401r_ccs.obj: ../startup_msp432p401r_ccs.c
-startup_msp432p401r_ccs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
-
-../startup_msp432p401r_ccs.c: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
